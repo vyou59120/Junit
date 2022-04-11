09a6cb718735321a1cf977d306939e33eb83f1b6
vyou59120 <van-rottana.you@hotmail.com>
2022-04-11 07:59:53 +0200

    first commit

---
+++ TestSpringMVC/src/main/java/com/example/demo/TestSpringMvcApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringMvcApplication.class, args);
+	}
+}
